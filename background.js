78a5cf6ff51895f9ade862a65cd451635dd7b89e
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-08-21 02:10:37 +0530

    Added multiple functionalities ike background change, text reader, link highlighter, image read aloud etc

--- background.js
+++ background.js
@@ -29,3 +29,58 @@ for(let i = 0; i < images.length; i++) {
     }
     )
 }
+
+const textToSpeech = document.getElementsByClassName('text-to-speech');
+for (let i = 0; i < textToSpeech.length; i++) {
+    textToSpeech[i].addEventListener('click', function (e) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "text-to-speech"});
+        }
+        )
+    }
+    )
+}
+
+const links = document.getElementsByClassName('link');
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function (e) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "link-highlight"});
+        }
+        )
+    })
+};
+
+const imageReader = document.getElementsByClassName('img-read');
+for (let i = 0; i < imageReader.length; i++) {
+    imageReader[i].addEventListener('click', function (e) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "image-reader"});
+        }
+        )
+    }
+    )
+}
+
+const backgroundColorChanger = document.getElementById('backgroundColorChanger');
+backgroundColorChanger.addEventListener('submit', function (e) {
+    e.preventDefault();
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "backgroundColor", backgroundColor: e.target.color.value});
+    });
+});
+
+document.getElementsByClassName("revert-background-color")[0].addEventListener('click', function (e) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "revert-background-color" });
+    })
+});
+
+// backgroundColorChanger.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     console.log("1242121");
+//     // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     //     chrome.tabs.sendMessage(tabs[0].id, { action: "backgroundColor", backgroundColor: backgroundColorChanger.color.value });
+//     // }
+//     // )
+// });
--- webpage-assets/js/content.js
+++ webpage-assets/js/content.js
@@ -1,7 +1,11 @@
-window.addEventListener("load", function load(event){
+window.addEventListener("load", function load(event) {
+  let sp1 = '';
+
     window.removeEventListener("load", load, false);
   chrome.runtime.onMessage.addListener(
+  
     function (request, sender, sendResponse) {
+        let backColor = '';
       const action = request.action;
       if (action === 'fontSize') {
         const fontSize = request.fontSize;
@@ -38,6 +42,63 @@ window.addEventListener("load", function load(event){
         for (let i = 0; i < immgs.length; i++) {
           immgs[i].style.setProperty('display', 'none');
         }
+      }
+
+      else if(action === 'text-to-speech'){
+        const text = document.getElementsByTagName('body')[0].innerText;
+        const msg = new SpeechSynthesisUtterance(text);
+        sp1=window.speechSynthesis.speak(msg);
+      }
+
+      else if(action === 'text-to-speech-stop'){
+        sp1.cancel();
+      }
+
+      else if (action === 'link-highlight') {
+        const links = document.getElementsByTagName('a');
+        for (let i = 0; i < links.length; i++) {
+          links[i].style.setProperty('background-color', 'yellow');
+          links[i].style.setProperty('fontSize', '24px');
+        }
+      }
+
+      else if (action === 'link-highlight-stop') {
+        const links = document.getElementsByTagName('a');
+        for (let i = 0; i < links.length; i++) {
+          links[i].style.setProperty('background-color', 'transparent');
+          links[i].style.setProperty('fontSize', 'default');
+        }
+      }
+
+      else if (action === 'image-reader') {
+        
+        const images = document.getElementsByTagName('img');
+        console.log(images)
+        for (let i = 0; i < images.length; i++) {
+          //read out alt text on mouse hover
+
+
+          images[i].addEventListener('mouseover', function (e) {
+            console.log("here")
+            console.log(images[i].alt);
+            const msg = new SpeechSynthesisUtterance(images[i].alt);
+            window.speechSynthesis.speak(msg);
+          });  
+
+          //mouse exit
+          images[i].addEventListener('mouseleave', function (e) {
+            window.speechSynthesis.cancel();
+          });  
+        }
+      }
+
+      else if (action === "backgroundColor") {
+        backColor===''?backColor = document.getElementsByTagName('body')[0].style.backgroundColor:null;
+        document.getElementsByTagName('body')[0].style.setProperty('background-color', request.backgroundColor);
+      }
+
+      else if(action === "revert-background-color") {
+        document.getElementsByTagName('body')[0].style.setProperty('background-color', backColor);
       }
     }
   );
