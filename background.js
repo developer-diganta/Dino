c95168c6df63aeb35f6af939aac7960c0ef773a1
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-08-20 18:21:18 +0530

    Added font style change and remove images

--- background.js
+++ background.js
@@ -1,10 +1,31 @@
-const btns = document.getElementsByTagName('button');
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', function (e) {
+const fontBtns = document.getElementsByClassName('fontSize');
+for (let i = 0; i < fontBtns.length; i++) {
+    fontBtns[i].addEventListener('click', function (e) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "fontSize", fontSize: e.target.innerText});
         });
     }
-)}
+    )
+}
+const fontStyleBtns = document.getElementsByClassName('fontStyle');
+for (let i = 0; i < fontStyleBtns.length; i++) {
+    fontStyleBtns[i].addEventListener('click', function (e) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "fontStyle", fontStyle: e.target.innerText});
+        }
+        )
+    }
+    )
+}
 
-console.log("hi")
+const images = document.getElementsByClassName('img-remmover');
+for(let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function (e) {
+        console.log("clicked")
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "image"});
+        }
+        )
+    }
+    )
+}
--- webpage-assets/js/content.js
+++ webpage-assets/js/content.js
@@ -9,6 +9,36 @@ window.addEventListener("load", function load(event){
         html.style.fontSize = fontSize;
         console.log(html.innerText);
       }
+      else if (action === 'fontStyle') {
+        console.log(request.fontStyle);
+        document.getElementsByTagName('body')[0].setAttribute('id', 'fonter');
+        const html = document.querySelector('#fonter');
+
+        if(request.fontStyle === 'Trebuchet MS') {
+          html.style.setProperty('font-family', 'Castellar, Trebuchet MS, sans-serif');
+        }
+        else if(request.fontStyle === 'Arial') {
+          html.style.setProperty('font-family', 'Arial, sans-serif');
+        }
+        else if(request.fontStyle === 'Baskerville') {
+          html.style.setProperty('font-family', 'Baskerville, serif');
+        }
+        else if(request.fontStyle === 'Calibri') {
+          html.style.setProperty('font-family', 'Calibri, sans-serif');
+        }
+        else if (request.fontStyle === 'Garamond') {
+          html.style.setProperty('font-family', 'Garamond, serif');
+        }
+        else if(request.fontStyle === 'Verdana') {
+          html.style.setProperty('font-family', 'Verdana, sans-serif');
+        }
+      }
+      else if (action === 'image') {
+        const immgs = document.getElementsByTagName('img');
+        for (let i = 0; i < immgs.length; i++) {
+          immgs[i].style.setProperty('display', 'none');
+        }
+      }
     }
   );
 },false);
