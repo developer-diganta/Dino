73e09022a0190cd836972b6a757b0340901a6295
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-08-21 02:24:49 +0530

    Added icon

--- background.js
+++ background.js
@@ -76,11 +76,3 @@ document.getElementsByClassName("revert-background-color")[0].addEventListener('
     })
 });
 
-// backgroundColorChanger.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     console.log("1242121");
-//     // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     //     chrome.tabs.sendMessage(tabs[0].id, { action: "backgroundColor", backgroundColor: backgroundColorChanger.color.value });
-//     // }
-//     // )
-// });
