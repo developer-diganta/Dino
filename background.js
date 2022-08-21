64f0df14719c231577bea4c6c0e5dc27ffb40348
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-08-21 15:50:37 +0530

    Removed revert colors

--- background.js
+++ background.js
@@ -117,11 +117,11 @@ fontColorChanger.addEventListener('submit', function (e) {
     });
 });
 
-document.getElementsByClassName("revert-font-color")[0].addEventListener('click', function (e) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "revert-font-color" });
-    })
-});
+// document.getElementsByClassName("revert-font-color")[0].addEventListener('click', function (e) {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, { action: "revert-font-color" });
+//     })
+// });
 
 
 const highlightPara = document.getElementsByClassName('para-highlighter');
