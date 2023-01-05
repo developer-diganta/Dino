33a9b8489bced68131a987c39cd64ff820dd33f3
sumit <sumitsantosh555@gmail.com>
2023-01-05 21:19:10 +0530

    Add Dark Mode in a extensions

--- background.js
+++ background.js
@@ -187,3 +187,15 @@ selectedText[0].addEventListener("click", function () {
     );
   });
 });
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
