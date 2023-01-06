58ce4383fb6cf31ed0b484f001a845f6e41feee6
Nehaa3121 <nehachoudhary3198@gmail.com>
2023-01-06 13:57:07 +0530

    added revert font color button

--- background.js
+++ background.js
@@ -114,6 +114,18 @@ document
     });
   });
 
+  
+  document
+  .getElementsByClassName("revert-Font-color")[0]
+  .addEventListener("click", function (e) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "revert-Font-color",
+      });
+    });
+  });
+
+
 const fontColorChanger = document.getElementById("fontColorChanger");
 fontColorChanger.addEventListener("submit", function (e) {
   e.preventDefault();
