ac9caadd9daf4f6f686568ef907fabba3760d45f
sumit <sumitsantosh555@gmail.com>
2023-01-07 09:33:24 +0530

    Remove eventlistener

--- background.js
+++ background.js
@@ -193,9 +193,3 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
-
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
-})
