6786b0b4c4a1b7703ec7eeae166530a88451f19d
Mridul Mittal <mridulmittal2003@gmail.com>
2023-01-21 15:29:41 +0530

    Added case Converter

--- background.js
+++ background.js
@@ -376,6 +376,22 @@ for (let i = 0; i < italics_underscore_reset.length; i++) {
     });
   });
 }
+// Usage
+// Add click event listeners to buttons
+document.getElementById("upper-case-btn").addEventListener("click", function() {
+  convertCase("uppercase");
+});
+document.getElementById("lower-case-btn").addEventListener("click", function() {
+  convertCase("lowercase");
+});
+
+// Background script
+function convertCase(caseType) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action:"convertCase",payload: caseType});
+  });
+}
+
 
 const savePreferenceBtn = document
   .getElementById("savePreferenceBtn")
