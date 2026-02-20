c30b335c9a6409a759c080d717cfecba0fd40fe2
Ifraah Tabassum <81137447+IT0506@users.noreply.github.com>
2026-02-20 08:59:27 +0530

    modified common.js

--- popup/common.js
+++ popup/common.js
@@ -1,1 +1,16 @@
 let Preference = {};
+function saveSettings(settings) {
+  chrome.storage.sync.set({ accessibilitySettings: settings });
+}
+
+function loadSettings(callback) {
+  chrome.storage.sync.get(["accessibilitySettings"], function (result) {
+    const defaultSettings = {
+      fontSize: "16px",
+      highContrast: false,
+      highlightLinks: false
+    };
+
+    callback(result.accessibilitySettings || defaultSettings);
+  });
+}
